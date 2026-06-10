8887f38497ed6bbc35740919a2413c820ba3a610
Rajeevkumar69 <rk488296@gmail.com>
2026-06-11 01:46:51 +0530

    Start Pattern

---
+++ star_pattern.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+     for (int i = 1; i <= 5; i++)
+     {
+          for (int j = 1; j <= 5; j++)
+          {
+               if (j >= 6 - i)
+               {
+                    cout << "*";
+               }
+               else
+               {
+                    cout << " ";
+               }
+          }
+          cout << "\n";
+     }
+     return 0;
+
+     for (int i = 1; i <= 5; i++)
+     {
+          for (int j = 1; j <= 5; j++)
+          {
+               if (j <= 6 - i)
+               {
+                    cout << "*";
+               }
+               else
+               {
+                    cout << " ";
+               }
+          }
+          cout << "\n";
+     }
+     return 0;
+
+     for (int i = 1; i <= 5; i++)
+     {
+          for (int j = 1; j <= 5; j++)
+          {
+               if (j >= i)
+               {
+                    cout << "*";
+               }
+               else
+               {
+                    cout << " ";
+               }
+          }
+          cout << "\n";
+     }
+     return 0;
+}
