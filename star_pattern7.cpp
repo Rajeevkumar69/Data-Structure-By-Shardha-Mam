060bab899e7fa28fe34541ebe3d7dd8b90a5926c
Rajeevkumar69 <rk488296@gmail.com>
2026-06-14 23:21:37 +0530

    Star Pattern 6 & 7

---
+++ star_pattern7.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+     int k;
+     for (int i = 1; i <= 4; i++)
+     {
+          k = 1;
+          for (int j = 1; j <= 7; j++)
+          {
+               if (j >= 5 - i && j <= 3 + i)
+               {
+                    cout << k;
+                    if (j < 4)
+                    {
+                         k++;
+                    }
+                    else
+                    {
+                         k--;
+                    }
+               }
+               else
+               {
+                    cout << " ";
+               }
+          }
+          cout << "\n";
+     }
+     return 0;
+}
