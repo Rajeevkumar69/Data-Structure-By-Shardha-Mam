298b113418df6baef62a7e97e3b8929fdc30204a
Rajeevkumar69 <rk488296@gmail.com>
2026-06-17 01:09:52 +0530

    Two pointers approach

--- dsa/reverse_array.cpp
+++ dsa/reverse_array.cpp
@@ -9,12 +9,30 @@ void reverseArray(int arr[], int size)
      }
 }
 
+//  Two pointers approach
+
+void reversseArr(int arr[], int size)
+{
+     int start = 0, end = size - 1;
+     while (start < end)
+     {
+          swap(arr[start], arr[end]);
+          start++;
+          end--;
+     }
+}
+
 int main()
 {
      int numsArr[] = {10, 20, 30, 60};
      int size = sizeof(numsArr) / sizeof(numsArr[0]);
 
      reverseArray(numsArr, size);
+     reversseArr(numsArr, size);
+     for (int i = 0; i < size; i++)
+     {
+          cout << numsArr[i] << " ";
+     }
 
      return 0;
 }
