a77f1ac776b16aaca58b6be12fff319e99497c85
Rajeevkumar69 <rk488296@gmail.com>
2026-06-19 23:31:02 +0530

    dsa

--- dsa/sum_one_d_array.cpp
+++ dsa/sum_one_d_array.cpp
@@ -8,12 +8,12 @@ int main()
 
      for (int i = 1; i < nums.size(); i++)
      {
-          nums[i] += nums[i - 1];
+          nums[i] = nums[i] + nums[i - 1];
      }
 
-     for (int num : nums)
+     for (int value : nums)
      {
-          cout << num << " ";
+          cout << value << " ";
      }
 
      return 0;
