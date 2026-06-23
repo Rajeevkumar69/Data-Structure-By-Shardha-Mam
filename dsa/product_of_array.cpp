cf2f3a05129e94200854ddb757d12427fd905ce2
Rajeevkumar69 <rk488296@gmail.com>
2026-06-24 00:59:24 +0530

    product of array

--- dsa/product_of_array.cpp
+++ dsa/product_of_array.cpp
@@ -5,9 +5,9 @@ using namespace std;
 vector<int> productOfArray(vector<int> nums)
 {
      int size = nums.size();
+
      vector<int> ans(size, 1);
-     vector<int> prefix(size, 1);
-     vector<int> suffix(size, 1);
+     int suffix = 1;
 
      if (nums.empty())
      {
@@ -16,16 +16,14 @@ vector<int> productOfArray(vector<int> nums)
 
      for (int i = 1; i < size; i++)
      {
-          prefix[i] = prefix[i - 1] * nums[i - 1];
+          ans[i] = ans[i - 1] * nums[i - 1];
      }
      for (int i = size - 2; i >= 0; i--)
      {
-          suffix[i] = suffix[i + 1] * nums[i + 1];
+          suffix *= nums[i + 1];
+          ans[i] *= suffix;
      }
-     for (int i = 0; i < size; i++)
-     {
-          ans[i] = prefix[i] * suffix[i];
-     }
+
      return ans;
 }
 
