047384eee24a989894ce4172b13385f096b32198
Rajeevkumar69 <rk488296@gmail.com>
2026-06-25 01:13:59 +0530

    pointers Operation

--- dsa/pointers/basic_pointers.cpp
+++ dsa/pointers/basic_pointers.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+void pointerOperation()
+{
+     int arr[] = {10, 20, 30, 40};
+     int *ptr = arr;
+
+     cout << *(ptr + 1) << endl;
+     cout << *(ptr + 3) << endl;
+     ptr++;
+     cout << *ptr << endl;
+}
+
 int main()
 {
      int a = 5;
@@ -12,10 +23,13 @@ int main()
      cout << **q << endl;
      cout << *q << endl;
 
+     // Pointer Arithmetic
      cout << p << endl;
      p++;
      // p--;
      cout << p << endl;
+
+     pointerOperation();
 
      return 0;
 }
