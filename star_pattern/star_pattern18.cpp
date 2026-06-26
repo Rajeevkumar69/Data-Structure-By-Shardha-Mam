ad6c6239948e03692a3fdb7ef5a5da4735f665dc
Rajeevkumar69 <rk488296@gmail.com>
2026-06-26 23:28:02 +0530

    star pattern 18

---
+++ star_pattern/star_pattern18.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+     for (int i = 1; i <= 4; i++)
+     {
+          char k = 'A';
+          for (int j = 1; j <= 7; j++)
+          {
+               if (j >= 5 - i && j <= 3 + i)
+               {
+                    cout << k;
+                    j < 4 ? k++ : k--;
+               }
+               else
+               {
+                    cout << " ";
+               }
+          }
+          cout << "\n";
+     }
+     return 0;
+}
