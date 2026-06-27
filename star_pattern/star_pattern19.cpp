d364f8f93837f2d860a5f056abebcfa8ef8be1ec
Rajeevkumar69 <rk488296@gmail.com>
2026-06-27 23:09:27 +0530

    star pattern 19

---
+++ star_pattern/star_pattern19.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+     for (int i = 1; i <= 4; i++)
+     {
+          char k = 'A';
+          int l = 1;
+          for (int j = 1; j <= 8; j++)
+          {
+               if (j >= 5 - i && j <= 4 + i)
+               {
+                    if (j < 5)
+                    {
+                         cout << k;
+                         k++;
+                    }
+                    else
+                    {
+                         cout << l;
+                         l++;
+                    }
+               }
+               else
+               {
+                    cout << " ";
+               }
+          }
+          cout << "\n";
+     }
+     return 0;
+}
