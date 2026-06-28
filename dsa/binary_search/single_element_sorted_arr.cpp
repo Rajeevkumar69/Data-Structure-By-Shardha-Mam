661772470c2f14ecdcf78bd78bad346f30685e69
Rajeevkumar69 <rk488296@gmail.com>
2026-06-29 01:57:11 +0530

    code modifed

--- dsa/binary_search/single_element_sorted_arr.cpp
+++ dsa/binary_search/single_element_sorted_arr.cpp
@@ -4,36 +4,39 @@ using namespace std;
 
 int findSignleElementInSortedArray(vector<int> arr)
 {
-     int size = arr.size(), start = 0, end = size - 1;
+     int n = arr.size();
+
+     if (n == 1)
+          return arr[0];
+
+     if (arr[0] != arr[1])
+          return arr[0];
+
+     if (arr[n - 1] != arr[n - 2])
+          return arr[n - 1];
+
+     int start = 1, end = n - 2;
+
      while (start <= end)
      {
           int mid = start + (end - start) / 2;
 
-          if (arr[mid - 1] != arr[mid] && arr[mid] != arr[mid + 1])
-          {
+          if (arr[mid] != arr[mid - 1] && arr[mid] != arr[mid + 1])
                return arr[mid];
-          }
+
           if (mid % 2 == 0)
           {
                if (arr[mid] == arr[mid + 1])
-               {
                     start = mid + 2;
-               }
                else
-               {
-                    end = mid;
-               }
+                    end = mid - 1;
           }
           else
           {
                if (arr[mid] == arr[mid - 1])
-               {
                     start = mid + 1;
-               }
                else
-               {
                     end = mid - 1;
-               }
           }
      }
      return -1;
