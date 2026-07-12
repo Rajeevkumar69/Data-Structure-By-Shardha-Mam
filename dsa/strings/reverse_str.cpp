7dcce94c7a1d6b700f336d48aeef76f0dc0ffddb
Rajeevkumar69 <rk488296@gmail.com>
2026-07-13 02:07:22 +0530

    basic string

---
+++ dsa/strings/reverse_str.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+     string str = "Chamcham";
+
+     // for (int i = str.length(); i >= 0; i--)
+     // {
+     //      cout << str[i];
+     // }
+
+     // Two pointers approach
+     // int st = 0, end = str.size() - 1;
+
+     // while (st < end)
+     // {
+     //      swap(str[st], str[end]);
+     //      st++;
+     //      end--;
+     // }
+
+     reverse(str.begin(), str.end());
+     cout << str;
+
+     return 0;
+}
