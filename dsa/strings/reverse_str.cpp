18fe57d150f372566f61477fdd3018c5fcd78337
Rajeevkumar69 <rk488296@gmail.com>
2026-07-14 22:36:40 +0530

    code

--- dsa/strings/reverse_str.cpp
+++ dsa/strings/reverse_str.cpp
@@ -8,22 +8,22 @@ int main()
 
      string str = "Chamcham";
 
-     // for (int i = str.length(); i >= 0; i--)
-     // {
-     //      cout << str[i];
-     // }
+     for (int i = str.length(); i >= 0; i--)
+     {
+          cout << str[i];
+     }
 
      // Two pointers approach
-     // int st = 0, end = str.size() - 1;
+     int st = 0, end = str.size() - 1;
 
-     // while (st < end)
-     // {
-     //      swap(str[st], str[end]);
-     //      st++;
-     //      end--;
-     // }
+     while (st < end)
+     {
+          swap(str[st], str[end]);
+          st++;
+          end--;
+     }
 
-     reverse(str.begin(), str.end());
+     // reverse(str.begin(), str.end());
      cout << str;
 
      return 0;
