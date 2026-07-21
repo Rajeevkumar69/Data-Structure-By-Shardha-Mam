63aa42138358315a67daf807b67e0018a7a4d1fd
Rajeevkumar69 <rk488296@gmail.com>
2026-07-22 00:15:41 +0530

    calculate the total sum of digits

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int main()
 {
+     int num = 12366;
+     int sum = 0;
+     for (int i = 0; i < 5; i++)
+     {
+          int lastDigit = num % 10;
+          sum += lastDigit;
+          num = num / 10;
+     }
+     cout << "Total: " << sum << "\n";
      cout << "Hello Chamcham Gupta";
      return 0;
 }
