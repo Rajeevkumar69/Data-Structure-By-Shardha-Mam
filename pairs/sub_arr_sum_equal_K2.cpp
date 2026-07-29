d9c023b12442538d37f8be1304ab3d13a551edc6
Rajeevkumar69 <rk488296@gmail.com>
2026-07-29 22:58:26 +0530

    Count Sum Array Sum Equals K :: Optmized Approach

--- pairs/sub_arr_sum_equal_K2.cpp
+++ pairs/sub_arr_sum_equal_K2.cpp
@@ -5,9 +5,45 @@ Sub array sum equals K: Optimal Approach
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 using namespace std;
+
+int countSubArrSum(vector<int> nums, int k)
+{
+     int n = nums.size(), count = 0;
+     vector<int> prefixSum(n, 0);
+
+     prefixSum[0] = nums[0];
+
+     for (int i = 1; i < n; i++)
+     {
+          prefixSum[i] = prefixSum[i - 1] + nums[i];
+     }
+
+     unordered_map<int, int> m;
+
+     for (int j = 0; j < n; j++)
+     {
+          if (prefixSum[j] == k)
+               count++;
+
+          int val = prefixSum[j] - k;
+
+          if (m.find(val) != m.end())
+          {
+               count += m[val];
+          }
+
+          m[prefixSum[j]]++;
+     }
+     return count;
+}
 
 int main()
 {
+     vector<int> arr = {9, 4, 11, 3, 6, 16};
+     int tar = 33;
+
+     cout << countSubArrSum(arr, tar);
      return 0;
 }
