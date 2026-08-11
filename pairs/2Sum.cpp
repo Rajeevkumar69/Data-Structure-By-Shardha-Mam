fbd10eb6ec586a4cd0691442838f0359ba27f191
Rajeevkumar69 <rk488296@gmail.com>
2026-08-11 23:55:42 +0530

    2 Sum revision

--- pairs/2Sum.cpp
+++ pairs/2Sum.cpp
@@ -17,12 +17,41 @@ void twoSum(vector<int> nums, int n, int target)
      }
 }
 
+void two_sum(vector<int> &nums, int n, int target)
+{
+     int start = 0, end = n - 1;
+     bool found = false;
+
+     while (start < end)
+     {
+
+          int sum = nums[start] + nums[end];
+
+          if (sum == target)
+          {
+               cout << start << "," << end;
+               found = true;
+               break;
+          }
+          else if (sum < target)
+          {
+               start++;
+          }
+          else
+          {
+               end--;
+          }
+     }
+     if (!found)
+          cout << -1 << "," << -1;
+}
+
 int main()
 {
-     vector<int> arr = {1, 3, 5, 0};
+     vector<int> arr = {1, 3, 5, 7};
      int size = arr.size();
-     int target = 1;
+     int target = 121;
 
-     twoSum(arr, size, target);
+     two_sum(arr, size, target);
      return 0;
 }
