b1a4d7e64507ea6bfa43ca48318de0d194ed5293
Rajeevkumar69 <rk488296@gmail.com>
2026-08-26 23:59:46 +0530

    Pratice 6

---
+++ practice/practice_6.cpp
@@ -0,0 +1,37 @@
+//  Flatten 2D array
+vector<vector<int>> mat = {{1, 2}, {3, 6}};
+
+int n = mat.size(), m = mat[0].size();
+vector<int> ans;
+
+for (int i = 0; i < n; i++)
+{
+
+     for (int j = 0; j < m; j++)
+     {
+
+          ans.push_back(mat[i][j]);
+     }
+}
+return ans;
+
+// Transpose 2D Matrix
+
+vector<vector<int>> mat = {{1, 2}, {2, 6}};
+
+int n = mat.size(), m = mat[0].size();
+
+if (n != m)
+{
+     return;
+}
+
+for (int i = 0; i < n; i++)
+{
+
+     for (int j = i + 1; j < m; j++)
+     {
+
+          swap(mat[i][j], mat[j][i]);
+     }
+}
