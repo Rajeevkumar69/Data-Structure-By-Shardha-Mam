2ebd2734afba91790bd6bc1a71678de22e6bcaaa
Rajeevkumar69 <rk488296@gmail.com>
2026-08-27 00:04:36 +0530

    Practice 7

---
+++ practice/practice_7.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+     // Two Sum
+     vector<int> arr = {2, 2, 4, 6, 6, 5, 5};
+     vector<int> ans;
+     int n = arr.size();
+
+     for (int i = 0; i < n; i++)
+     {
+          int count = 0;
+
+          for (int j = i + 1; j < n; j++)
+          {
+               if (arr[i] == arr[j])
+               {
+                    count++;
+               }
+          }
+
+          if (count == 1)
+          {
+               ans.push_back(arr[i]);
+          }
+     }
+
+     //  Remove duplicate from array
+     int unique = 0;
+
+     for (int i = 0; i < n; i++)
+     {
+          unique ^= arr[i];
+     }
+
+     return unique;
+}
