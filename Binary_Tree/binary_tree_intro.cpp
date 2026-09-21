f1062a3a3190204e56da7ba8313237a0e32db628
Rajeevkumar69 <rk488296@gmail.com>
2026-09-22 00:26:35 +0530

    Binary Tree

--- Binary_Tree/binary_tree_intro.cpp
+++ Binary_Tree/binary_tree_intro.cpp
@@ -119,14 +119,13 @@ int main()
 
      Node *root = buildTree(preOrder);
 
-     // cout << root->data << "\n";
+     cout << root->data << "\n";
 
-     // cout << root->right->left->data << "\n";
+     cout << root->right->left->data << "\n";
 
-     // preOrderTraversal(root);
-     // inOrderTraversal(root);
-     // postOrderTraversal(root);
-
+     preOrderTraversal(root);
+     inOrderTraversal(root);
+     postOrderTraversal(root);
      levelOrderTraversal(root);
 
      return 0;
