49d50c8c7c3c77f4e9ea9896a2f452226c747b1d
Rajeevkumar69 <rk488296@gmail.com>
2026-09-22 00:26:10 +0530

    Level Order Traversal

--- Binary_Tree/binary_tree_intro.cpp
+++ Binary_Tree/binary_tree_intro.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 class Node
@@ -73,19 +74,64 @@ void postOrderTraversal(Node *root)
      cout << root->data << " ";
 }
 
+// Level Order Traversal
+
+void levelOrderTraversal(Node *root)
+{
+     queue<Node *> q;
+     q.push(root);
+     q.push(NULL);
+
+     while (q.size() > 0)
+     {
+          Node *curr = q.front();
+          q.pop();
+
+          if (curr == NULL)
+          {
+               if (!q.empty())
+               {
+                    cout << "\n";
+                    q.push(NULL);
+                    continue;
+               }
+               else
+               {
+                    break;
+               }
+          }
+          cout << curr->data << " ";
+
+          if (curr->left != NULL)
+          {
+               q.push(curr->left);
+          }
+          if (curr->right != NULL)
+          {
+               q.push(curr->right);
+          }
+     }
+}
+
 int main()
 {
      vector<int> preOrder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
 
      Node *root = buildTree(preOrder);
 
-     cout << root->data << "\n";
+     // cout << root->data << "\n";
 
-     cout << root->right->left->data << "\n";
+     // cout << root->right->left->data << "\n";
 
-     preOrderTraversal(root);
-     inOrderTraversal(root);
-     postOrderTraversal(root);
+     // preOrderTraversal(root);
+     // inOrderTraversal(root);
+     // postOrderTraversal(root);
+
+     levelOrderTraversal(root);
 
      return 0;
 }
+
+/*
+TC: O(n)
+*/
