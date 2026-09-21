e01e8836c939bf5ca8a9ea61112eb0c0538b20cb
Rajeevkumar69 <rk488296@gmail.com>
2026-09-21 22:52:56 +0530

    BInary Tree Intro

---
+++ Binary_Tree/binary_tree_intro.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node
+{
+public:
+     int data;
+     Node *left;
+     Node *right;
+
+     Node(int val)
+     {
+          data = val;
+          left = right = NULL;
+     }
+};
+
+static int idx = -1;
+
+Node *buildTree(vector<int> preOrder)
+{
+     idx++;
+
+     if (preOrder[idx] == -1)
+     {
+          return NULL;
+     }
+
+     Node *root = new Node(preOrder[idx]);
+     root->left = buildTree(preOrder);
+     root->right = buildTree(preOrder);
+
+     return root;
+}
+
+int main()
+{
+     vector<int> preOrder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+
+     Node *root = buildTree(preOrder);
+
+     cout << root->data << "\n";
+
+     cout << root->right->left->data;
+
+     return 0;
+}
