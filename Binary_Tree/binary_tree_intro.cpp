76b0ac6abca5058302464fe8a38272232c74b043
Rajeevkumar69 <rk488296@gmail.com>
2026-09-21 23:39:36 +0530

    Pre order Traversal

--- Binary_Tree/binary_tree_intro.cpp
+++ Binary_Tree/binary_tree_intro.cpp
@@ -34,15 +34,30 @@ Node *buildTree(vector<int> preOrder)
      return root;
 }
 
+// Preorder Traversal
+
+void preOrderTraversal(Node *root)
+{
+     if (root == NULL)
+     {
+          return;
+     }
+     cout << root->data << " ";
+     preOrderTraversal(root->left);
+     preOrderTraversal(root->right);
+}
+
 int main()
 {
      vector<int> preOrder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
 
      Node *root = buildTree(preOrder);
 
-     cout << root->data << "\n";
+     // cout << root->data << "\n";
 
-     cout << root->right->left->data;
+     // cout << root->right->left->data;
+
+     preOrderTraversal(root);
 
      return 0;
 }
